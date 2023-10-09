91d9df080eff3e1611a4983a6463805b1454bf7e
ryohey <info@codingcafe.jp>
2023-10-09 10:47:04 +0900

    Init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Zoomable
+
 A SwiftUI view modifier that provides pinch to zoom, double tap to zoom, and drag to pan functionalities.
+
+## Features
+
+- Pinch to zoom
+- Double tap to zoom in and out
+- Drag to pan
+
+## Installation
+
+### Swift Package Manager
+
+Please add the following URL in the `Package Dependencies` screen in Xcode.
+
+```
+https://github.com/ryohey/Zoomable.git
+```
+
+## Usage
+
+To use the `Zoomable` modifier in your SwiftUI view:
+
+```swift
+import YourLibraryName
+
+struct ContentView: View {
+    var body: some View {
+        Image("your-image-name")
+            .zoomable()
+    }
+}
+```
+
+## Requirements
+
+- iOS 16.0 or later
+- SwiftUI
+
+## Caveats
+
+- **iOS 16**: Due to limitations with `MagnificationGesture`, during pinch-in actions, the zoom location is fixed to the top-left corner.
+- **iOS 17 and later**: This issue has been addressed and improved with the introduction of `MagnifyGesture`.
+
+## Contribution
+
+Contributions are welcome! Please open an issue or submit a pull request.
+
+## License
+
+MIT License
---
+++ Sources/Zoomable/Zoomable.swift
@@ -0,0 +1,192 @@
+#if os(iOS)
+
+import SwiftUI
+
+struct ZoomableModifier: ViewModifier {
+    let minZoomScale: CGFloat
+    let doubleTapZoomScale: CGFloat
+
+    @State private var lastTransform: CGAffineTransform = .identity
+    @State private var transform: CGAffineTransform = .identity
+    @State private var imageSize: CGSize = .zero
+
+    func body(content: Content) -> some View {
+        content
+            .background(alignment: .topLeading) {
+                GeometryReader { proxy in
+                    Color.clear
+                        .onAppear {
+                            imageSize = proxy.size
+                        }
+                }
+            }
+            .animatableTransformEffect(transform)
+            .gesture(dragGesture, including: transform == .identity ? .none : .all)
+            .modify { view in
+                if #available(iOS 17.0, *) {
+                    view.gesture(magnificationGesture)
+                } else {
+                    view.gesture(oldMagnificationGesture)
+                }
+            }
+            .gesture(doubleTapGesture)
+    }
+
+    @available(iOS, introduced: 16.0, deprecated: 17.0)
+    private var oldMagnificationGesture: some Gesture {
+        MagnificationGesture()
+            .onChanged { value in
+                let zoomFactor = 0.5
+                let scale = value * zoomFactor
+                transform = lastTransform.scaledBy(x: scale, y: scale)
+            }
+            .onEnded { _ in
+                onEndGesture()
+            }
+    }
+
+    @available(iOS 17.0, *)
+    private var magnificationGesture: some Gesture {
+        MagnifyGesture(minimumScaleDelta: 0)
+            .onChanged { value in
+                let newTransform = CGAffineTransform.anchoredScale(
+                    scale: value.magnification,
+                    anchor: value.startAnchor.scaledBy(imageSize)
+                )
+
+                withAnimation(.interactiveSpring) {
+                    transform = lastTransform.concatenating(newTransform)
+                }
+            }
+            .onEnded { _ in
+                onEndGesture()
+            }
+    }
+
+    private var doubleTapGesture: some Gesture {
+        SpatialTapGesture(count: 2)
+            .onEnded { value in
+                let newTransform: CGAffineTransform =
+                    if transform.isIdentity {
+                        .anchoredScale(scale: doubleTapZoomScale, anchor: value.location)
+                    } else {
+                        .identity
+                    }
+
+                withAnimation(.linear(duration: 0.15)) {
+                    transform = newTransform
+                    lastTransform = newTransform
+                }
+            }
+    }
+
+    private var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                withAnimation(.interactiveSpring) {
+                    transform = lastTransform.translatedBy(
+                        x: value.translation.width / transform.scaleX,
+                        y: value.translation.height / transform.scaleY
+                    )
+                }
+            }
+            .onEnded { _ in
+                onEndGesture()
+            }
+    }
+
+    private func onEndGesture() {
+        let newTransform = limitTransform(transform)
+
+        withAnimation(.snappy(duration: 0.1)) {
+            transform = newTransform
+            lastTransform = newTransform
+        }
+    }
+
+    private func limitTransform(_ transform: CGAffineTransform) -> CGAffineTransform {
+        let scaleX = transform.scaleX
+        let scaleY = transform.scaleY
+
+        if scaleX < minZoomScale
+            || scaleY < minZoomScale
+        {
+            return .identity
+        }
+
+        let maxX = imageSize.width * (scaleX - 1)
+        let maxY = imageSize.height * (scaleY - 1)
+
+        if transform.tx > 0
+            || transform.tx < -maxX
+            || transform.ty > 0
+            || transform.ty < -maxY
+        {
+            let tx = min(max(transform.tx, -maxX), 0)
+            let ty = min(max(transform.ty, -maxY), 0)
+            var transform = transform
+            transform.tx = tx
+            transform.ty = ty
+            return transform
+        }
+
+        return transform
+    }
+}
+
+public extension View {
+    @ViewBuilder
+    func zoomable(
+        minZoomScale: CGFloat = 1,
+        doubleTapZoomScale: CGFloat = 3
+    ) -> some View {
+        modifier(ZoomableModifier(
+            minZoomScale: minZoomScale,
+            doubleTapZoomScale: doubleTapZoomScale
+        ))
+    }
+}
+
+private extension View {
+    @ViewBuilder
+    func modify(@ViewBuilder _ fn: (Self) -> some View) -> some View {
+        fn(self)
+    }
+
+    @ViewBuilder
+    func animatableTransformEffect(_ transform: CGAffineTransform) -> some View {
+        scaleEffect(
+            x: transform.scaleX,
+            y: transform.scaleY,
+            anchor: .zero
+        )
+        .offset(x: transform.tx, y: transform.ty)
+    }
+}
+
+private extension UnitPoint {
+    func scaledBy(_ size: CGSize) -> CGPoint {
+        .init(
+            x: x * size.width,
+            y: y * size.height
+        )
+    }
+}
+
+private extension CGAffineTransform {
+    static func anchoredScale(scale: CGFloat, anchor: CGPoint) -> CGAffineTransform {
+        CGAffineTransform(translationX: anchor.x, y: anchor.y)
+            .scaledBy(x: scale, y: scale)
+            .translatedBy(x: -anchor.x, y: -anchor.y)
+    }
+
+    var scaleX: CGFloat {
+        sqrt(a * a + c * c)
+    }
+
+    var scaleY: CGFloat {
+        sqrt(b * b + d * d)
+    }
+}
+
+#endif
